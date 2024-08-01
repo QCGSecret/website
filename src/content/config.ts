aa4171bd73583afde0c013a4e684b6208f66eda7
Ecorte <me@ecorte.xyz>
2024-08-01 11:50:05 -0400

    refactor(Guides): migrate to content collection

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+import { defineCollection, reference, z } from 'astro:content';
+
+const guideCollection = defineCollection({
+	type: 'content',
+	schema: z.object({
+		id: z.string(),
+		map: z.string(),
+		authors: z.array(reference('author')),
+		wip: z.boolean().default(false),
+	}),
+});
+
+const authorCollection = defineCollection({
+	type: 'data',
+	schema: z.object({
+		name: z.string(),
+		avatar: z.string(),
+	}),
+});
+
+export const collections = {
+	guide: guideCollection,
+	author: authorCollection,
+};
